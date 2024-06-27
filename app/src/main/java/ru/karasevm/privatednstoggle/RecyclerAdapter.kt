c24f8e0b38425ddd7cdf74aa2151b00d891b013a
Praveen Kumar <68219142+InfiniteCoder06@users.noreply.github.com>
2024-06-27 17:15:05 +0530

    Feature: Reordring (#23)
    * Reordering of Servers
    
    * Fix drag handle being drawn outside of the screen
    
    ---------
    
    Co-authored-by: Maksim Karasev <karasevm98@gmail.com>

--- app/src/main/java/ru/karasevm/privatednstoggle/RecyclerAdapter.kt
+++ app/src/main/java/ru/karasevm/privatednstoggle/RecyclerAdapter.kt
@@ -1,15 +1,21 @@
 package ru.karasevm.privatednstoggle
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.Collections
 
-class RecyclerAdapter(private val items: MutableList<String>) :
+class RecyclerAdapter(private val items: MutableList<String>, private val showDragHandle: Boolean) :
     RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     var onItemClick: ((Int) -> Unit)? = null
+    var onItemsChanged: ((MutableList<String>) -> Unit)? = null
+    var onDragStart: ((RecyclerAdapter.ViewHolder) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val view =
@@ -18,20 +24,39 @@ class RecyclerAdapter(private val items: MutableList<String>) :
         return vh
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
         val item = items[position]
 
         // sets the text to the textview from our itemHolder class
         holder.textView.text = item
+        if(showDragHandle) {
+            holder.dragHandle.visibility = View.VISIBLE
+            holder.dragHandle.setOnTouchListener {
+                    _, event ->
+                if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+                    onDragStart?.invoke(holder)
+                }
+                return@setOnTouchListener true
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
+    fun onItemMove(fromPosition: Int, toPosition: Int) {
+        // Swap items in your data list
+        Collections.swap(items, fromPosition, toPosition)
+        notifyItemMoved(fromPosition, toPosition)
+        onItemsChanged?.invoke(items)
+    }
+
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView: TextView = itemView.findViewById(R.id.textView)
+        val dragHandle: ImageView = itemView.findViewById(R.id.dragHandle)
 
         init {
             itemView.setOnClickListener {
