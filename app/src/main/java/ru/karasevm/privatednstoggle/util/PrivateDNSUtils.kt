e477df4cf5347cbe5cac2dfbe81f6bac47ac13e8
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2025-02-09 15:57:24 +0530

    Add Shortcuts

--- app/src/main/java/ru/karasevm/privatednstoggle/util/PrivateDNSUtils.kt
+++ app/src/main/java/ru/karasevm/privatednstoggle/util/PrivateDNSUtils.kt
@@ -3,11 +3,18 @@ package ru.karasevm.privatednstoggle.util
 import android.Manifest
 import android.content.ContentResolver
 import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.provider.Settings
+import android.widget.Toast
 import androidx.core.content.ContextCompat.checkSelfPermission
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import ru.karasevm.privatednstoggle.R
+import ru.karasevm.privatednstoggle.data.DnsServerRepository
+import ru.karasevm.privatednstoggle.model.DnsServer
+import ru.karasevm.privatednstoggle.util.PreferenceHelper.autoMode
 
-@Suppress("unused")
 object PrivateDNSUtils {
     const val DNS_MODE_OFF = "off"
     const val DNS_MODE_AUTO = "opportunistic"
@@ -23,12 +30,12 @@ object PrivateDNSUtils {
 
 
     // Gets the system dns mode
-    fun getPrivateMode(contentResolver: ContentResolver): String {
+    fun getPrivateMode(contentResolver: ContentResolver): String? {
         return Settings.Global.getString(contentResolver, PRIVATE_DNS_MODE)
     }
 
     // Gets the system dns provider
-    fun getPrivateProvider(contentResolver: ContentResolver): String {
+    fun getPrivateProvider(contentResolver: ContentResolver): String? {
         return Settings.Global.getString(contentResolver, PRIVATE_DNS_PROVIDER)
     }
 
@@ -43,10 +50,72 @@ object PrivateDNSUtils {
     }
 
     fun checkForPermission(context: Context): Boolean {
-        return checkSelfPermission(
-            context,
-            Manifest.permission.WRITE_SECURE_SETTINGS
-        ) == PackageManager.PERMISSION_GRANTED
+        if (checkSelfPermission(
+                context,
+                Manifest.permission.WRITE_SECURE_SETTINGS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        Toast.makeText(context, R.string.permission_missing, Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    /**
+     * Gets next dns address from the database,
+     * if current address is last or unknown returns null
+     *
+     * @param currentAddress currently set address
+     * @return next address
+     */
+    suspend fun getNextAddress(
+        repository: DnsServerRepository,
+        currentAddress: String?
+    ): DnsServer? {
+        return if (currentAddress.isNullOrEmpty()) {
+            repository.getFirstEnabled()
+        } else {
+            repository.getNextByServer(currentAddress)
+        }
+    }
+
+    fun getNextProvider(
+        sharedPrefs: SharedPreferences,
+        scope: CoroutineScope,
+        repository: DnsServerRepository,
+        contentResolver: ContentResolver,
+        skipProvider: Boolean = false,
+        onNext: ((String, String?) -> Unit)
+    ) {
+        val dnsMode = getPrivateMode(contentResolver)
+        val dnsProvider = getPrivateProvider(contentResolver)
+
+        if (dnsMode.equals(DNS_MODE_OFF, ignoreCase = true)) {
+            if (sharedPrefs.autoMode == AUTO_MODE_OPTION_AUTO || sharedPrefs.autoMode == AUTO_MODE_OPTION_OFF_AUTO) {
+                onNext.invoke(DNS_MODE_AUTO, dnsProvider)
+            } else {
+                onNext.invoke(DNS_MODE_PRIVATE, dnsProvider)
+            }
+
+        } else if (dnsMode == null || dnsMode.equals(DNS_MODE_AUTO, ignoreCase = true)) {
+            onNext.invoke(DNS_MODE_PRIVATE, null)
+        } else if (dnsMode.equals(DNS_MODE_PRIVATE, ignoreCase = true)) {
+            scope.launch {
+                if (getNextAddress(repository, dnsProvider) == null) {
+                    if (!skipProvider && sharedPrefs.autoMode == AUTO_MODE_OPTION_PRIVATE) {
+                        onNext.invoke(DNS_MODE_PRIVATE, null)
+                    } else {
+                        if (sharedPrefs.autoMode == AUTO_MODE_OPTION_AUTO) {
+                            onNext.invoke(DNS_MODE_AUTO, dnsProvider)
+                        } else {
+                            onNext.invoke(DNS_MODE_OFF, dnsProvider)
+                        }
+                    }
+                } else {
+                    onNext.invoke(DNS_MODE_OFF, dnsProvider)
+                }
+            }
+        }
     }
 
 }
